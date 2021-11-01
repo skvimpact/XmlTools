398f1d4e3b50bf217208a8b5a94628125c565675
skvimpact <skvimpact@yandex.ru>
2021-11-01 15:12:22 +0300

    Add project files.

---
+++ Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace XmlTools
+{
+    public class Model
+    {
+        public static string ToXML<T>(T obj)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (StringWriter stringWriter = new StringWriter(new StringBuilder()))
+            {                
+                xmlSerializer.Serialize(stringWriter, obj);
+                return stringWriter.ToString();
+            }
+        }
+    }
+}
---
+++ Xml.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+namespace XmlTools
+{
+    public class Xml
+    {
+        public static T ToModel<T>(string xml)
+        {
+            XmlSerializer xsSubmit = new XmlSerializer(typeof(T));
+            using (var sr = new StringReader(xml))
+            {
+                return (T)xsSubmit.Deserialize(sr);
+            }
+        }
+    }
+}
